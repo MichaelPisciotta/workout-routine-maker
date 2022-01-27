fde4e4c457937d5d2d60b43c9d081810296137a5
MichaelPisciotta <mfp56@miami.edu>
2022-01-26 19:48:17 -0500

    created login method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
 
     def login
+       user = User.find_by(username: params[:user][:username])
+       if user && user.authenticate(params[:user][:password])
+        #create session
+        session[:user_id] = user.id
         byebug
-    end
-end
+        render json: {message: "login successful", user: user}, status: 200
+       else
+        render json: {error: "Invalid username and/or password, please try again."}, status: :unauthorized 
+       end
+
+    end #end of login method
+
+end #end of controller
